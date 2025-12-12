0ee3fda8f36bfa970658119f36b09813fddcbb40
Daniel <pinilla.garcia.daniel@gmail.com>
2025-12-12 14:57:59 +0100

    update

--- process_data.py
+++ process_data.py
@@ -12,14 +12,75 @@ def main(data_file_path = None,**kwargs):
 
     args = {**default,**kwargs}
 
-    df_data = pd.read_excel(data_file_path, decimal = ',')
+    df = pd.read_excel(data_file_path, decimal = ',')
 
     for columna in args['to_datetime_columns']:
-        df_data[columna] = pd.to_datetime(df_data[columna], errors = 'coerce')
+        df[columna] = pd.to_datetime(df[columna], errors = 'coerce')
 
-    df_data =df_data.drop(args['drop_columns'],axis=1)
+    df = df.drop(args['drop_columns'],axis=1)
 
+    df_data = pd.DataFrame(columns = ['id', 'days','event','diameter']) 
+
+    # ====================== PARTICULAR DATA PROCESSING FOR THE STUDY ====================== 
+
+
+    df_analysis = df[df['Valido'] == True].copy()
+    # print(df.columns.to_list())
+
+    df_analysis['cirugia'] = df_analysis['fecha_cirugia'].notna()
+    df_analysis['embolia'] = df_analysis['fecha_trat1'].notna()
+    df_analysis['muerte'] = df_analysis['fecha_muerte'].notna()
+    df_analysis['previa'] = df_analysis['fecha_prev1'].notna()
+
+    df_analysis = df_analysis.drop(['Valido','result','PatientID','fecha_trat2'], axis=1)
+
+    for index, row in df_analysis.iterrows():
     
+        lista_fechas = [ row['fecha_cirugia'], row['fecha_alta'], row['fecha_muerte']]
+        lista_eventos = [ 'ciru', 'alta', 'muer']
+
+        if pd.notna(row['fecha_trat1']) and row['fecha_trat1'] >= row['fecha_eco']: # solo tener en cuenta fechas embolias posteriores a la embolia o iguales
+            
+            lista_fechas.insert(0, row['fecha_trat1']) # importante insertarlo al principio por si las fechas son las mismas, no puede morir antes de una embolia
+            lista_eventos.insert(0, 'embo')
+
+        fecha_prev_max = max((f for f in [row['fecha_prev1'], row['fecha_prev2'], row['fecha_prev3']] if pd.notna(f)), default=pd.NaT)
+        if pd.notna(fecha_prev_max) and fecha_prev_max > row['fecha_eco']: # solo tener en cuenta fechas embolias posteriores a la embolia
+            
+            lista_fechas.insert(0, fecha_prev_max) # importante insertarlo al principio por si las fechas son las mismas, no puede morir antes de una embolia
+            lista_eventos.insert(0, 'prev')
+
+        fecha_min = min(fecha for fecha in lista_fechas if pd.notna(fecha))
+        fecha_temprana = lista_eventos[lista_fechas.index(fecha_min)]
+
+        fila_data = {'id': row['record_id'],'diameter': row['Final'],'days': (fecha_min - row['fecha_eco']).days}
+
+        if fecha_temprana == 'prev':
+            fila_data['event'] = 1
+
+        elif fecha_temprana == 'embo': 
+            fila_data['event'] = 1
+            
+        elif fecha_temprana == 'ciru':
+            fila_data['event'] = 0
+
+        elif fecha_temprana == 'muer': 
+            fila_data['event'] = 0
+
+        elif fecha_temprana == 'alta':
+            fila_data['event'] = 0
+        
+        else: 
+            print('error:',row['record_id'])
+            
+        df_data.loc[len(df_data)] = fila_data
+
+        #print(fecha_temprana,fila_data)
+
+
+    # ====================================================================================== 
+
+
 
     return df_data
     
--- run.py
+++ run.py
@@ -6,8 +6,28 @@ def main(data_file_path):
     to_datetime_columns = ['fecha_muerte', 'fecha_alta', 'fecha_cirugia','fecha_eco','fecha_trat1','fecha_prev1','fecha_prev2', 'fecha_prev3']
     drop_columns =['comments','revisar medida','diam_ao', 'diam_mitral','diam_ao_2','diam_mitral_2','indicacion_cirugia','fecha_diagnostico']
     
-    df_data = process_data.main(data_file_path, drop_columns = drop_columns, to_datetime_columns= to_datetime_columns)
-    print(df_data)
+    df_data = process_data.main(
+        data_file_path,
+        drop_columns = drop_columns,
+        to_datetime_columns=to_datetime_columns
+        )
+    
+    df_cases =  df_data[ df_data['event']==1]
+    df_controls = df_data[df_data['event']==0]
+
+    print('Cases:',df_cases.shape[0])
+    print('Controls:',df_controls.shape[0])
+
+    mean_cases = df_cases['diameter'].mean()
+    std_cases = df_cases['diameter'].std()
+
+    mean_controls = df_controls['diameter'].mean()
+    std_controls = df_controls['diameter'].std()
+
+    print("Mean Final Cases:", mean_cases)
+    print("Std Final Cases:", std_cases)
+    print("Mean Final Controls:", mean_controls)
+    print("Std Final Controls:", std_controls)
     
 
 if __name__ == '__main__':
